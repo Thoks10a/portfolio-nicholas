d5435024487b8d27e5ded0b1f967980b81e05450
Thoks10a <nicholas.isola@hotmail.com>
2021-12-30 14:39:25 -0300

    New NavBar
    NavBar melhorada, com ícones!

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -2,6 +2,8 @@
 // Import Routes React
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
+// CSS
+import './style.css';
 //Import Routes Project
 import Home from '../home/home';
 import About from '../about/about';
@@ -11,6 +13,8 @@ import Videos from '../videos/Videos';
 import Projetos from '../projetos/projetos';
 
 import ScrollToTop from './ScrollToTop';
+
+import { IconContext } from 'react-icons';
 
 import {
     Navbar,
@@ -32,19 +36,47 @@ import { Link } from 'react-router-dom';
 
 import { useState, useEffect } from 'react';
 
+// Icons
+import {
+    AiFillGithub,
+    AiFillLinkedin,
+    AiOutlineInstagram,
+    AiFillHome,
+    AiOutlinePlus,
+    AiFillVideoCamera,
+    AiFillProject,
+} from 'react-icons/ai';
+
+import { SiAboutdotme } from 'react-icons/si';
+import { DiGithub } from 'react-icons/di';
+
+import logo from '../image/logo.png';
+
 const PortStyle = {
-    'margin-right': '100px',
+    'margin-right': '300px',
 };
 
 const linkStyle = {
-    color: 'gray',
+    // color: 'gray',
     'font-family': 'helvetica',
     'text-decoration': 'none',
-    'margin-left': '10px',
+    // 'margin-left': '10px',
 };
 
 const tilespace = {
-    'margin-left': '20px',
+    // 'margin-left': '20px',
+    width: '40px',
+    height: '40px',
+};
+
+const icon = {
+    padding: '5px 5px 5px 5px',
+    margin: 0,
+    marginRight: 50,
+    // borderColor: 'rgba(0, 0, 0, 0)',
+    opacity: '0.5',
+    // 'background-color': 'rgb(83,83,83)',
+    // transition: '0s background-color',
 };
 
 const Routes = () => {
@@ -57,64 +89,149 @@ const Routes = () => {
                 <div>
                     <Navbar expand="md" className="navbar" light>
                         <NavbarBrand href="/" style={PortStyle}>
-                            Portfolio Nicholas
+                            <img src={logo} alt="" width="80"></img>
                         </NavbarBrand>
                         <NavbarToggler onClick={toggle} />
                         <Collapse isOpen={isOpen} navbar>
-                            <Nav className="me-auto" navbar>
-                                <NavItem style={tilespace}>
+                            <Nav className="me-auto " navbar>
+                                <NavItem
+                                    style={tilespace}
+                                    className="icon-navbar-hover"
+                                >
                                     {/* <NavLink href="/">Home</NavLink> */}
                                     <Link
                                         to={'/'}
                                         href="#foto"
                                         style={linkStyle}
+                                        className="a-button"
                                     >
+                                        <AiFillHome size={25} />
                                         Home
                                     </Link>
                                 </NavItem>
-                                <NavItem style={tilespace}>
+                                <NavItem
+                                    style={tilespace}
+                                    className="icon-navbar-hover"
+                                >
                                     {/* <NavLink href="/about">About</NavLink> */}
-                                    <Link to={'/about'} style={linkStyle}>
-                                        Sobre
+                                    <Link
+                                        to={'/about'}
+                                        style={linkStyle}
+                                        className="a-button"
+                                    >
+                                        <SiAboutdotme
+                                            size={25}
+                                            className="icon-navbar-hover"
+                                        />
+                                        About
                                     </Link>
                                 </NavItem>
-                                <NavItem style={tilespace}>
+                                <NavItem
+                                    style={tilespace}
+                                    className="icon-navbar-hover"
+                                >
                                     {/* <NavLink href="/githubprojects">
                                         GitHub Projects
                                     </NavLink> */}
                                     <Link
                                         to={'/githubprojects'}
                                         style={linkStyle}
+                                        className="a-button"
                                     >
+                                        <DiGithub
+                                            size={25}
+                                            className="icon-navbar-hover"
+                                        />
                                         GitHub
                                     </Link>
                                 </NavItem>
-                                <NavItem style={tilespace}>
+                                <NavItem
+                                    style={tilespace}
+                                    className="icon-navbar-hover"
+                                >
                                     {/* <NavLink href="/UmPoucoMais">
                                         Um pouco mais
                                     </NavLink> */}
-                                    <Link to={'/UmPoucoMais'} style={linkStyle}>
-                                        Um pouco mais
+                                    <Link
+                                        to={'/UmPoucoMais'}
+                                        style={linkStyle}
+                                        className="a-button"
+                                    >
+                                        <AiOutlinePlus
+                                            size={25}
+                                            className="icon-navbar-hover"
+                                        />
+                                        Mais
                                     </Link>
                                 </NavItem>
-                                <NavItem style={tilespace}>
+                                <NavItem
+                                    style={tilespace}
+                                    className="icon-navbar-hover"
+                                >
                                     {/* <NavLink href="/UmPoucoMais">
                                         Um pouco mais
                                     </NavLink> */}
-                                    <Link to={'/videos'} style={linkStyle}>
+                                    <Link
+                                        to={'/videos'}
+                                        style={linkStyle}
+                                        className="a-button"
+                                    >
+                                        <AiFillVideoCamera
+                                            size={25}
+                                            className="icon-navbar-hover"
+                                        />
                                         Videos
                                     </Link>
                                 </NavItem>
-                                <NavItem style={tilespace}>
+                                <NavItem
+                                    style={tilespace}
+                                    className="icon-navbar-hover"
+                                >
                                     {/* <NavLink href="/UmPoucoMais">
                                         Um pouco mais
                                     </NavLink> */}
-                                    <Link to={'/projetos'} style={linkStyle}>
+                                    <Link
+                                        to={'/projetos'}
+                                        style={linkStyle}
+                                        className="a-button"
+                                    >
+                                        <AiFillProject
+                                            size={25}
+                                            className="icon-navbar-hover"
+                                        />
                                         Projetos
                                     </Link>
                                 </NavItem>
                             </Nav>
                         </Collapse>
+
+                        <Button
+                            type="button"
+                            className="btn-hover"
+                            style={icon}
+                            href="https://github.com/Thoks10a?tab=repositories"
+                            target="_blank"
+                        >
+                            <AiFillGithub size={25} />
+                        </Button>
+                        <Button
+                            type="button"
+                            className="btn-hover"
+                            style={icon}
+                            href="https://www.linkedin.com/in/nicholas-silva-8100a2184/"
+                            target="_blank"
+                        >
+                            <AiFillLinkedin size={25} />
+                        </Button>
+                        <Button
+                            type="button"
+                            className="btn-hover"
+                            style={icon}
+                            href="https://www.instagram.com/nicholas_29183/?hl=pt-br"
+                            target="_blank"
+                        >
+                            <AiOutlineInstagram size={25} />
+                        </Button>
                     </Navbar>
                 </div>
                 <Route component={Home} path="/" exact />
